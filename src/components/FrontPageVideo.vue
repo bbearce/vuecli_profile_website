a081745ccfbdebddcbd3ff8cd07a4113427c3148
Benjamin <bbearce@gmail.com>
2021-12-01 21:41:11 -0500

    src as video tag attr

--- src/components/FrontPageVideo.vue
+++ src/components/FrontPageVideo.vue
@@ -1,7 +1,8 @@
 <template>
-    <video id="background-video" autoplay loop muted poster="https://assets.codepen.io/6093409/river.jpg">
+    <!-- <video id="background-video" autoplay="autoplay" loop muted poster="https://assets.codepen.io/6093409/river.jpg">
       <source src="https://assets.codepen.io/6093409/river.mp4" type="video/mp4">
-    </video>
+    </video> -->
+    <video id="background-video" autoplay="autoplay" loop muted poster="https://assets.codepen.io/6093409/river.jpg"  src="https://assets.codepen.io/6093409/river.mp4"></video>
 </template>
 
 <script>
