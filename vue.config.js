61c607ff04b46a4ea01d9fcc3166f16854291886
bbearce <bbearce@gmail.com>
2021-12-22 01:12:09 -0500

    trying out historyApiFallback

--- vue.config.js
+++ vue.config.js
@@ -10,12 +10,20 @@ module.exports = {
         },
         experiment: {
             entry: "src/pages/experiment/main.js",
-            template: "public/index.html",
+            template: "public/experiment.html",
             filename: "experiment.html",
             title: "Experiment Page",
             chunks: ['chunk-vendors', 'chunk-common', 'experiment']
         },
     },
+    devServer: {
+        historyApiFallback: {
+          rewrites: [
+            { from: /\/index/, to: '/index.html' },
+            { from: /\/experiment/, to: '/experiment.html' }
+          ]
+        }
+      },
     // https://www.youtube.com/watch?v=agaC4oKn_0k
     css: {
         loaderOptions: {
