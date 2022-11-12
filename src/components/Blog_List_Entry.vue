0de35546d69fef085c4c3f4daaba8bf4cde1001b
Benjamin Bearce <bbearce@gmail.com>
2022-11-12 15:33:59 -0500

    post 11/3

--- src/components/Blog_List_Entry.vue
+++ src/components/Blog_List_Entry.vue
@@ -3,12 +3,12 @@
   <div>
     <div class="card">
       <h2>{{ title }}</h2>
-      <h4>{{ author }}</h4>
-      <h5>{{ desc }} {{ date }}</h5>
+      <h3>{{ desc }}</h3>
+      <h4>{{ date }}</h4>
+      <h5>{{ author }}</h5>
       <a :href="'/main_site/blog_post/'+this.year+'-'+this.month+'-'+this.day">
-        <img class="fakeimg" v-bind:src="'/blog_posts/'+this.year+'/'+this.month+'/'+this.day+'/logo.png'" style="height:200px;">
+        <img class="blog_entry_cover_image" v-bind:src=image style="height:200px;">
       </a>
-      <p>{{ desc }}</p>
     </div>
   </div>
 
@@ -26,25 +26,26 @@ export default {
             "date": String,
             "image": String,
     },
-    created() {
+  created() {
     },
     data() {
         return {
             year: this.date.slice(0,4),
             month: this.date.slice(5,7),
-            day: this.date.slice(8,10),
-
+            day: this.date.slice(8, 10)
         }
     }
 }
 </script>
 
 <style scoped>
-/* Fake image */
-.fakeimg {
+h3 {
+  text-align: center;
+}
+.blog_entry_cover_image {
   background-color: rgb(87, 87, 87);
-  width: 30%;
-  width: 40%;
+  width: 50%;
+  /* height: 40%; */
   padding: 10px;
 }
 
@@ -53,20 +54,44 @@ export default {
   background-color: #f1f3f4;
   padding: 20px;
   /* margin: 0px 200px 100px 200px; */
-  margin: 0% 10% 10% 10%;
+  margin: 0% 33% 10% 30%;
   border-radius: 1%;
   box-shadow: -2px 1px 8px 1px lightgrey;
 }
 
 @media only screen and (min-width: 1300px) {
   .card {
-    margin: 0% 20% 10% 20%;
+    margin: 0% 28% 10% 28%;
+  }
+}
+
+@media only screen and (min-width: 1400px) {
+  .card {
+    margin: 0% 30% 10% 30%;
+  }
+}
+
+@media only screen and (min-width: 1500px) {
+  .card {
+    margin: 0% 32% 10% 32%;
+  }
+}
+
+@media only screen and (min-width: 1600px) {
+  .card {
+    margin: 0% 34% 10% 34%;
+  }
+}
+
+@media only screen and (min-width: 1700px) {
+  .card {
+    margin: 0% 36% 10% 36%;
   }
 }
 
 @media only screen and (min-width: 1800px) {
   .card {
-    margin: 0% 30% 5% 30%;
+    margin: 0% 38% 5% 38%;
   }
 }
 </style>
---
+++ src/utils/generate_slideshow_from_images.py
@@ -0,0 +1,84 @@
+from gcloud import storage
+client = storage.Client()  # Implicit environ set-up
+
+bucket_name = 'vuecli-profile-website'
+bucket = client.bucket(bucket_name)
+
+blobs_prefix = "blog_posts/2022/11/02/resized_"
+blobs_prefix = "blog_posts/2022/11/03/resized_"
+blobs_prefix = "blog_posts/2022/11/04/resized_"
+blobs_prefix = "blog_posts/2022/11/05/resized_"
+blobs_prefix = "blog_posts/2022/11/06/resized_"
+blobs_prefix = "blog_posts/2022/11/07/resized_"
+blobs_prefix = "blog_posts/2022/11/08/resized_"
+blobs_prefix = "blog_posts/2022/11/09/resized_"
+
+images = [i forTotal i in bucket.list_blobs(prefix=blobs_prefix)]
+count = len(images)
+image_links = [i.public_url for i in images]
+
+# Practice
+# 
+# image_name = image_links[0].replace(f"https://storage.googleapis.com/{bucket_name}/" + blobs_prefix.replace("/", "%2F"), "").replace(".jpg", "").replace(".png", "")
+
+# i = 1
+# count = 1
+# image_html = f"""
+# <div class="mySlides fade">
+#   <div class="numbertext">{i} / {count}</div>
+#   <img src=\"{image_links[0]}\" style=\"width:100%\">
+#   <div class="text">{image_name}</div>
+# </div>
+
+# """
+
+# print(image_html)
+
+images_html = ""
+dots = ""
+for i, image_url in enumerate(image_links, start=1):
+    image_name = image_url.replace(f"https://storage.googleapis.com/{bucket_name}/" + blobs_prefix.replace(
+        "/", "%2F"), "").replace(".jpg", "").replace(".png", "")
+    
+    image_html = f"""
+<div class="mySlides fade">
+    <div class="numbertext">{i} / {count}</div>
+    <img src=\"{image_url}\" style=\"width:100%\">
+    <div class="text">{image_name}</div>
+</div>
+
+    """
+    
+    dot = f"""<span class="dot" onclick="currentSlide({i})"></span> 
+    """
+    
+    images_html += image_html
+    dots += dot
+    
+
+
+slide_show_html = f"""
+
+<div class="slideshow-container">
+<!-- https://www.w3schools.com/howto/howto_js_slideshow.asp -->
+
+{images_html}
+
+
+<a class="prev" onclick="plusSlides(-1)">❮</a>
+<a class="next" onclick="plusSlides(1)">❯</a>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  {dots}
+</div>
+
+"""
+
+# print(slide_show_html)
+
+with open("src/utils/slide_show.html", "w") as file:
+    file.write(slide_show_html)
+
